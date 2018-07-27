90e995268f7ff7dceb11051f25c842825049bf8e
tushar-arote-hdfc <rajkumar.p@hdfc.com>
2018-07-27 17:10:11 +0530

    Million dollar commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -9,15 +9,10 @@ import { Pro } from '@ionic/pro';
 export class HomePage {
 
   constructor(public navCtrl: NavController) {
-  console.log('Changed for Master branch');
+  console.log('Changed for crm branch');
   //  console.error('testing');
   //Pro.monitoring.exception(new Error('error'))
- 
    // FGXSFXGSDXY
-   
-
-  
-    
    // Pro.monitoring.log('EDG'+" : "+'Now it is changed', { level: 'error' });
 
     // Pro.monitoring.call(() => {
