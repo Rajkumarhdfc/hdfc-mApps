2ad8da09108769c87f86058fcde964d7837f5f68
tushar-arote-hdfc <rajkumar.p@hdfc.com>
2018-08-03 14:29:58 +0530

    redirection test

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { NavController,LoadingController } from 'ionic-angular';
 import { Pro } from '@ionic/pro';
 
 import { InAppBrowser } from '@ionic-native/in-app-browser';
@@ -13,7 +13,7 @@ import { normalizeURL } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(private iab: InAppBrowser,public navCtrl: NavController) {
+  constructor(private iab: InAppBrowser,public loadingCtrl: LoadingController,public navCtrl: NavController) {
   console.log('Changed for crm branch');
 
   
@@ -32,12 +32,81 @@ export class HomePage {
     // })
 
   }
+
+
+  items = [
+    'crm',
+    'Phone book',
+    'Technical Appraisal',
+    'File Approval',
+    'Master'
+  ];
+
+
+  itemSelected(item: string) {
+    const versions = await Pro.deploy.getAvailableVersions();
+    console.log("Selected Item", item);
+    const app_path= this.seekChannels(versions, item);
+    console.log(app_path);
+    var path = 'file:///data/user/0/io.ionic.starter/files/ionic_built_snapshots/'+app_path+'/index.html';
+    const browser = this.iab.create(path, '_self');
+    console.log('launched successfully');
+  }
+
+
+
+
   async configureDeploy() {
     const config = {
       'appId': '040d0d97',
       'channel': 'Master'
     }
     await Pro.deploy.configure(config);
+  }
+
+  async performManualUpdatecrm(channel) {
+
+    const versions = await Pro.deploy.getAvailableVersions();
+    console.log(versions);
+    const prevVersion="";
+    const config = {
+      'appId': '040d0d97',
+      'channel': channel,
+      
+    }
+   
+    await Pro.deploy.configure(config);
+    
+  
+    if(versions.length>0){
+      const  prevVersion=await this.seekChannels(versions,channel);
+    }
+    const update = await Pro.deploy.checkForUpdate();
+    debugger;
+    if (update.available && update.snapshot !=prevVersion){
+      console.log('Fired for update');
+      let loading = this.loadingCtrl.create({
+        spinner: 'hide',
+        content: 'Downloading  '+channel
+      });
+      loading.present();
+
+    
+      await Pro.deploy.downloadUpdate((progress) => {
+        console.log(progress);
+      })
+      await Pro.deploy.extractUpdate((progress) => {
+        console.log(progress);
+      })
+      console.log(prevVersion);
+      if(prevVersion!=""){
+        Pro.deploy.deleteVersionById(prevVersion);
+      }
+      loading.dismiss();
+
+      //await Pro.deploy.reloadApp();
+    }
+    console.log('No update');
   }
   
   async performAutomaticUpdate() {
@@ -75,10 +144,12 @@ export class HomePage {
     const browser = this.iab.create(path, '_self');
   }
   
-  async performManualUpdatecrm() {
+
+  async performManualUpdatPB() {
     const config = {
       'appId': '040d0d97',
-      'channel': 'crm'
+      'channel': 'Phone book',
+      
     }
     await Pro.deploy.configure(config);
     const update = await Pro.deploy.checkForUpdate()
@@ -93,16 +164,59 @@ export class HomePage {
       //await Pro.deploy.reloadApp();
     }
   }
+  async performManualUpdateTA() {
+    const config = {
+      'appId': '040d0d97',
+      'channel': 'Technical Appraisal',
+      
+    }
+    await Pro.deploy.configure(config);
+    const update = await Pro.deploy.checkForUpdate()
+    debugger;
+    if (update.available){
+      await Pro.deploy.downloadUpdate((progress) => {
+        console.log(progress);
+      })
+      await Pro.deploy.extractUpdate((progress) => {
+        console.log(progress);
+      })
+      //await Pro.deploy.reloadApp();
+    }
+  }
+  async performManualUpdateFileApprovals() {
+    const config = {
+      'appId': '040d0d97',
+      'channel': 'File Approval',
+      
+    }
+    await Pro.deploy.configure(config);
+    const update = await Pro.deploy.checkForUpdate()
+    debugger;
+    if (update.available){
+      await Pro.deploy.downloadUpdate((progress) => {
+        console.log(progress);
+      })
+      await Pro.deploy.extractUpdate((progress) => {
+        console.log(progress);
+      })
+      //await Pro.deploy.reloadApp();
+    }
+  }
+
   async performManualUpdateMaster() {
     const config = {
       'appId': '040d0d97',
-      'channel': 'Master'
+      'channel': 'Master',
+    
     }
+    const versions = await Pro.deploy.getAvailableVersions();
+    console.log(versions);
+    const  prevVersion=await this.seekChannels(versions,'Master');
     await Pro.deploy.configure(config);
 
     const update = await Pro.deploy.checkForUpdate()
     debugger;
-    if (update.available){
+    if (update.available && update.snapshot !=prevVersion){
       await Pro.deploy.downloadUpdate((progress) => {
         console.log(progress);
       })
@@ -131,9 +245,69 @@ console.log(info);
     const info = await Pro.deploy.getCurrentVersion()
 console.log(info);
   }
+  async checkVersions() {
+    const versions = await Pro.deploy.getAvailableVersions();
+    console.log(versions);
+    // [
+    //   {
+    //     'versionId': 'versionId1',
+    //     'channel': 'CHANNEL_NAME',
+    //     'binaryVersion': '1.0.1'
+    //   },
+    //   {
+    //     'versionId': 'versionId2',
+    //     'channel': 'CHANNEL_NAME',
+    //     'binaryVersion': '1.0.1'
+    //   },
+    // ]
+  }
 
+  // function seekVendor(vendors, name) {
+  //   for (var i=0, l=vendors.length; i<l; i++) {
+  //     if (typeof vendors[i] == "object" && vendors[i].Name === name) {
+  //       return vendors[i];
+  //     }
+  //   }
+  // }
 
-  
+  // async deleteVersion(versions_id) {
+  // //  const versions = await Pro.deploy.getAvailableVersions();
+  //   Pro.deploy.deleteVersion(versions_id);
+  // }
  
+ 
+
+  seekChannels(versions, channel) {
+    for (var i=0, l=versions.length; i<l; i++) {
+      if (typeof versions[i] == "object" && versions[i].channel === channel) {
+        return versions[i].versionId;
+      }
+    }
+  }
+ 
+
+  async  ionViewDidEnter(){
+    console.log("ionViewDidEnter");
+  console.log(this.items.length);
+var packList= [
+  'crm',
+  'Phone book',
+  'Technical Appraisal',
+  'File Approval',
+  'Master'
+];
+  for(var i = 0; i < packList.length; i++){
+    console.log(packList[i])
+    // var channel_name= packList[i];
+    // console.log(channel_name);
+  await this.performManualUpdatecrm(packList[i])
+ }
+
+}
+async deleteVersion() {
+  const versions = await Pro.deploy.getAvailableVersions();
+  Pro.deploy.deleteVersionById(versions[0].versionId);
+}
+
 
 }
